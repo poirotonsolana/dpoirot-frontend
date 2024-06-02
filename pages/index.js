0436237b5f1ae56d0ee42f09dbc4e3fcc42c7eea
Matej DB <mdrazi01@fesb.hr>
2024-06-02 21:56:30 +0200

    font and contact

--- pages/index.js
+++ pages/index.js
@@ -2,6 +2,11 @@ import styles from '../styles/Home.module.css';
 import Image from 'next/image';
 
 export default function Home() {
+
+  const handleEmailSubmit = () => {
+    window.location.href = 'mailto:poirotonsolana@gmail.com?subject=Request%20for%20Audit&body=Hi%20there,%0D%0A%0D%0AI%20would%20like%20to%20request%20an%20audit%20for%20my%20project.%0D%0A%0D%0AThank%20you.';
+  }
+
   return (
     <div className={styles.fullScreenImageContainer}>
       <Image
@@ -13,8 +18,8 @@ export default function Home() {
       />
 
       <div className={styles.buttonsContainer}>
-        <button className={styles.button}>Enter APP</button>
-        <button className={styles.button}>Request an AUDIT</button>
+        <button onClick={handleEmailSubmit} className={styles.button}>Enter APP</button>
+        <button onClick={handleEmailSubmit} className={styles.button}>Request an AUDIT</button>
         <button className={styles.button}>Read DOCS</button>
       </div>
     </div>
